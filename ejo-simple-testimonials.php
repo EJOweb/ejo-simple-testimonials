7c7a852c7922acd37dd34a08aebd6a455d929b3f
EJOweb <erik@ejoweb.nl>
2015-08-08 15:23:14 +0200

    github-updater correct branch

--- ejo-simple-testimonials.php
+++ ejo-simple-testimonials.php
@@ -8,7 +8,7 @@
  * Author URI: http://www.ejoweb.nl/
  *
  * GitHub Plugin URI: http://github.com/ejoweb/ejo-simple-testimonials
- * GitHub Branch:     master
+ * GitHub Branch:     slaopbijstrang
  */
 
 /**
